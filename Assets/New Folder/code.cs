8602a855be115e41210cee3a05e9ec1cc68c0398
EsnH12 <113237880+EsnH12@users.noreply.github.com>
2025-06-19 18:27:39 +0300

    yeni for kodu
    deneme

---
+++ Assets/New Folder/code.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class code : MonoBehaviour
+{
+ 
+    void Update()
+    {
+        for( int i = 0; i < 10; i++ ) 
+        {
+        Debug.Log( " sayýlar" +  i );
+            i++;
+        }
+    }
+}
